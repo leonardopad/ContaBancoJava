d5dfa7aa6b5290d1bcf405bfea8e9389c84d0919
leonardopad <77343517+leonardopad@users.noreply.github.com>
2022-03-01 16:27:13 -0300

    Add files via upload

---
+++ ContaBancaria/src/com/github/leonardopad2/Programa.java
@@ -0,0 +1,23 @@
+package com.github.leonardopad2;
+
+
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		Conta conta1 = new Conta("João da Silva", 799.99, "123456", "PF");
+		conta1.sacar(100.0);
+		for(int i = 0; i < 3; i++) {
+			System.out.println("Sacou:" + (conta1.sacar(100.0))); 
+		}
+		conta1.depositar(100.0);
+		for(int i = 0; i > 3; i++) {
+			System.out.println("Depositou " + (conta1.depositar(100.0)));
+		}
+	}
+
+}
